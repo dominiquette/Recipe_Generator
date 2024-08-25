38c8c5fc41a6fcc9facd28acee44377221f42eed
dominiquette <dominique123p@gmail.com>
2024-08-25 12:34:27 +0100

    Added comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+# ===== Package Initialization ==========
+
+# Import the classes to be exposed when the package is imported
 from .app import SpoonacularAPI, RecipeFinder
 
+# Define the public API of the package
 __all__ = ['SpoonacularAPI', 'RecipeFinder']
